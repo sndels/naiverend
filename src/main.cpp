c1ba61f86080f106717dcdcd962d248c59c06e37
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2016-09-11 18:36:35 +0300

    Add simple glfw-window

---
+++ src/main.cpp
@@ -0,0 +1,107 @@
+#include "gl_core_4_1.h"
+#include <GLFW/glfw3.h>
+#include <memory>
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <sstream>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::cin;
+
+namespace {
+    const static GLsizei XRES = 1280;
+    const static GLsizei YRES = 720;
+    const static char* WINDOW_TITLE = "skunkwork";
+}
+
+static void errorCallback(int error, const char* description)
+{
+    cerr << description << endl;
+}
+
+static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main(void)
+{
+    // Init GLFW-context
+    glfwSetErrorCallback(errorCallback);
+    if (!glfwInit()) {
+        cerr << "Error initializing GLFW!" << endl;
+        cin.get();
+        exit(EXIT_FAILURE);
+    }
+
+    // Set desired context hints 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create the window
+    GLFWwindow* windowPtr;
+    windowPtr = glfwCreateWindow(XRES, YRES, WINDOW_TITLE, NULL, NULL);
+    if (!windowPtr) {
+        glfwTerminate();
+        cerr << "Error creating OpenGL window!" << endl;
+        cin.get();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(windowPtr);
+
+    // Load gl-functions (glLoadGen-header)
+    if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
+        cerr << "Error loading GL functions!" << endl;
+        cin.get();
+        glfwDestroyWindow(windowPtr);
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    GLenum error = glGetError();
+    if(error != GL_NO_ERROR) {
+        cerr << "GLError opening glfw context!" << endl;
+        cerr << "Error code: " << error << endl;
+        cin.get();
+        glfwDestroyWindow(windowPtr);
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    // Set input callback
+    glfwSetKeyCallback(windowPtr, keyCallback);
+
+    // Init GL settings
+    glViewport(0, 0, XRES, YRES);
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+
+    error = glGetError();
+    if(error != GL_NO_ERROR) {
+        cerr << "Error initializing OpenGL!" << endl;
+        cerr << "Error code: " << error << endl;
+        cin.get();
+        glfwDestroyWindow(windowPtr);
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    cout << glGetString(GL_VERSION) << endl;
+
+    // Run the main loop
+    uint64_t frameNum = 0;
+    while (!glfwWindowShouldClose(windowPtr)) {
+        ++frameNum;
+        glfwPollEvents();
+        glfwSwapBuffers(windowPtr);
+    }
+
+    // Release resources
+    glfwDestroyWindow(windowPtr);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
