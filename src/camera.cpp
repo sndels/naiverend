ce0359c314219cbff19d0dc31321ee7d69ac562e
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-01-28 21:31:17 +0200

    Fix perspective

--- src/camera.cpp
+++ src/camera.cpp
@@ -59,7 +59,7 @@ void Camera::setProj(const float& xres, const float& yres, const float& yFov, co
 {
     float ar = xres / yres;
     float tFov = (0.5f * yFov * PI_F / 180.f);
-    projM4f_ = mat4(1.f / ar * tFov,        0.f,               0.f, 0.f,
+    projM4f_ = mat4(1.f / (ar * tFov),        0.f,               0.f, 0.f,
                                 0.f, 1.f / tFov,               0.f, 0.f,
                                 0.f,        0.f,  (-zN-zF)/(zN-zF), 1.f,
                                 0.f,        0.f, 2.f*zF*zN/(zN-zF), 0.f );
